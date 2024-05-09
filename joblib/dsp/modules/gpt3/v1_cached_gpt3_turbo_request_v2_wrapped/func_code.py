6d4e76be6f66fa49a3b9967538fe6126298756bb
Shangyint <shangyin@berkeley.edu>
2024-05-09 10:08:37 -0700

    deleting stuff

--- joblib/dsp/modules/gpt3/v1_cached_gpt3_turbo_request_v2_wrapped/func_code.py
+++
@@ -1,5 +0,0 @@
-# first line: 253
-@functools.lru_cache(maxsize=None if cache_turn_on else 0)
-@NotebookCacheMemory.cache
-def v1_cached_gpt3_turbo_request_v2_wrapped(**kwargs):
-    return v1_cached_gpt3_turbo_request_v2(**kwargs)
